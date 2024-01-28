5b47fa045e58fd6dac8912327ef8eba7f63b016a
ValentynStarushok <l1nk3rino@gmail.com>
2024-01-28 23:41:37 +0200

    [HILL-024] use predefined words

--- hillel_hw_24/gen_random_word.py
+++ hillel_hw_24/gen_random_word.py
@@ -1,20 +1,24 @@
-from wonderwords import RandomWord
+import time
+import random
+import nltk
+from nltk.corpus import words
+
+nltk.download('words')
 
 def generate_random_words(num_words):
     if num_words > 10_000:
         raise ValueError("Number of words cannot exceed 10,000")
 
-    r = RandomWord()
-    unique_words = set()
-
-    while len(unique_words) < num_words:
-        # word = fake.word()
-        word = r.word()
-        if word not in unique_words:
-            unique_words.add(word)
-            yield word
-
+    word_list = list(words.words())
+    random.shuffle(word_list) # randomize
+    unique_words = set(word_list)
+    for word in list(unique_words)[:num_words]:
+        yield word
 
 num_words = 9_999
+start_time = time.perf_counter()
 for word in generate_random_words(num_words):
     print(word)
+
+end_time = time.perf_counter()
+print("Elapsed time: ", end_time - start_time)
